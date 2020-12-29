a0cea5f4250fdf880b44b1771cae678f3c9b72d5
hamidrouhi <hrouhi.kh@gmail.com>
2020-12-29 16:10:08 +0330

    modify base/urls.py

--- source/base/urls.py
+++ source/base/urls.py
@@ -1,21 +1,47 @@
-"""source URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.utils.translation import ugettext_lazy as _
 
-urlpatterns = [
+admin.site.site_header = _(f'{settings.SITE["NAME"]} administration')
+admin.site.site_title = _(f'{settings.SITE["DESCRIPTION"]} administration')
+admin.site.index_title = _('Dashboard')
+
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
+    prefix_default_language=False
+)
+
+urlpatterns += [
+
 ]
+
+# This is only needed when using runserver.
+if settings.DEBUG:
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework.permissions import AllowAny
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title=f'{settings.SITE["NAME"]} APIs',
+            default_version=f'{settings.REST_FRAMEWORK["DEFAULT_VERSION"]}',
+            description=f'{settings.SITE["DESCRIPTION"]}',
+            contact=openapi.Contact(email=f'{settings.DEFAULT_FROM_EMAIL}')
+        ),
+        public=True,
+        permission_classes=(AllowAny,)
+    )
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('api-auth',
+             include('rest_framework.urls', namespace='rest_framework')),
+        path('swagger', schema_view.with_ui('swagger', cache_timeout=0),
+             name='schema-swagger-ui'),
+        path('redoc', schema_view.with_ui('redoc', cache_timeout=0),
+             name='schema-redoc')
+    ]
