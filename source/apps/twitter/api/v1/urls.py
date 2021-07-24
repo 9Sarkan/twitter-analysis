26b87bb15cb09beeff51ad05a3d7b44d57093291
sarkan <sarkan.mohammady@hotmail.com>
2021-07-24 22:14:53 +0430

    urls added

--- source/apps/twitter/api/v1/urls.py
+++ source/apps/twitter/api/v1/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from .views import TestAPI
 
 
 router = DefaultRouter()
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("test/", TestAPI.as_view(), name="test"),
+]
--- source/apps/twitter/urls.py
+++ source/apps/twitter/urls.py
@@ -1,3 +1,3 @@
 from django.urls import path, include
 
-urlpatterns = [path("v1/twitter/", include("apps.twitter.api.v1.urls"))]
+urlpatterns = [path("", include("apps.twitter.api.v1.urls"))]
