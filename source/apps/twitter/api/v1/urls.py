385156ee23ac8e13e52f5a79c069bfa9c80fdbbc
sarkan <sarkan.mohammady@hotmail.com>
2021-09-18 20:58:47 +0430

    web services created

--- source/apps/twitter/api/v1/urls.py
+++ source/apps/twitter/api/v1/urls.py
@@ -1,11 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import TestAPI
+from . import views
 
-
-router = DefaultRouter()
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("test/", TestAPI.as_view(), name="test"),
+    path(
+        "get-most-commons/<str:tag>/",
+        views.GetMostCommons.as_view(),
+        name="get-most-commons",
+    ),
+    path(
+        "get-from-populars/<str:tag>/",
+        views.GetFromPopulars.as_view(),
+        name="get-from-populars",
+    ),
+    path(
+        "get-most-populars/<str:tag>/",
+        views.GetMostRetweeted.as_view(),
+        name="get-most-populars",
+    ),
 ]
